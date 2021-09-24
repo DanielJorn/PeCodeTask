89eac353db574f2380bacc9ef2cd97d4ec6c5d9a
Anton <0954264@gmail.com>
2021-09-24 09:44:47 +0300

    Added onPageChange listener

--- app/src/main/java/com/example/pecodetask/ViewPagerContainerFragment.kt
+++ app/src/main/java/com/example/pecodetask/ViewPagerContainerFragment.kt
@@ -1,19 +1,27 @@
 package com.example.pecodetask
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.pecodetask.databinding.FragmentViewPagerContainerBinding
+
+private const val TAG = "PagerContainerFragment"
 
 class ViewPagerContainerFragment : Fragment() {
     private var _binding: FragmentViewPagerContainerBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     private val pagerAdapter by lazy { ViewPagerAdapter(this) }
+    private val pageChangeCallback by lazy {
+        object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) =
+                this@ViewPagerContainerFragment.onPageSelected(position)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,10 +34,20 @@ class ViewPagerContainerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.viewPager.adapter = pagerAdapter
+        binding.viewPager.registerOnPageChangeCallback(pageChangeCallback)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.viewPager.unregisterOnPageChangeCallback(pageChangeCallback)
+    }
+
+    private fun onPageSelected(position: Int) {
+        Log.d(TAG, "onPageSelected: $position")
     }
 }
