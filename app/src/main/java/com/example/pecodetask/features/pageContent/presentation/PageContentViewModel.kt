98281001dc0a5c23a55b8bec5eded82435b58e59
Anton <0954264@gmail.com>
2021-09-25 15:30:18 +0300

    Reformat code

--- app/src/main/java/com/example/pecodetask/features/pageContent/presentation/PageContentViewModel.kt
+++ app/src/main/java/com/example/pecodetask/features/pageContent/presentation/PageContentViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.pecodetask.features.pageContent.presentation
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.pecodetask.features.pageContent.domain.NotificationRepository
 import com.example.pecodetask.features.pageContent.domain.model.NotificationData
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class PageContentViewModel @AssistedInject constructor(
     private val notificationRepository: NotificationRepository,
@@ -14,13 +15,11 @@ class PageContentViewModel @AssistedInject constructor(
 ) : ViewModel() {
 
     private val _notificationClick = MutableLiveData<NotificationData>()
-
     val notificationClick: LiveData<NotificationData> get() = _notificationClick
+
     fun onNewNotificationClick() {
-        viewModelScope.launch(Dispatchers.IO) {
-            val createdNotification = notificationRepository.saveNewNotification(pageNumber)
-            _notificationClick.postValue(createdNotification)
-        }
+        val createdNotification = notificationRepository.saveNewNotification(pageNumber)
+        _notificationClick.postValue(createdNotification)
     }
 
     @dagger.assisted.AssistedFactory
